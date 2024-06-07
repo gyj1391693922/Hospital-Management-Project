b78304b37865cf865c06f59aba21c11878b68d5e
英杰 <120712796+gyj1391693922@users.noreply.github.com>
2024-06-07 09:28:36 +0800

    Delete springboot directory

--- springboot/src/main/java/com/example/springboot/exception/ServiceException.java
+++
@@ -1,8 +0,0 @@
-package com.example.springboot.exception;
-
-public class ServiceException extends RuntimeException{
-
-    public ServiceException(String message) {
-        super(message);
-    }
-}
